5f4848c5d5a2cfab57bc93510df8badc151baefd
AliceRin29 <69286020+AliceRin29@users.noreply.github.com>
2020-08-13 11:33:35 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App(){
       height:100,
       padding:20
     }}>
-      <View style ={{backgroundColor:"red",flex:0.3}}/>
+      <View style ={{backgroundColor:"red",flex:0.5}}/>
       <View style ={{backgroundColor:"blue",flex:0.3}}/>
       <Text>หมูกรอบทอดกระเทียม อร่อยจริงๆ</Text>
     </View>
